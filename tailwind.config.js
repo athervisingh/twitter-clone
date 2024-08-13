fec4d64381effe6b1795fc14dbb1877beec567b6
athervisingh <athervisingh@gmail.com>
2024-08-13 05:40:14 +0530

    twitter

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*html"],
+  theme: {
+    extend: {
+      lineHeight: {
+        '16':'4rem',
+      }
+    },
+  },
+  plugins: [],
+}
+
